2e51eff575709407f0991a2eafd24642d203d56a
iYuuba21 <114842096+iYuuba21@users.noreply.github.com>
2024-04-14 21:58:08 +0700

    Add files via upload

---
+++ BangunDatar.java
@@ -0,0 +1,18 @@
+public abstract class BangunDatar {
+
+    String warna;
+
+    String getWarna() {
+
+        return warna;
+    }
+
+    void setwarna(String warna) {
+
+        this.warna = warna;
+
+    }
+
+    abstract float getLuas();
+
+}
